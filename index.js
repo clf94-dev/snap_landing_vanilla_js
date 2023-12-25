8eab72d5bece972c124b21af49727ae0c203c51b
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-25 15:36:40 +0100

    - fix burger animation - position navbar items mobile

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const shadowContainer = document.querySelector('.navbar-shadow')
-    const navLinks = document.querySelector('.nav-links li');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         shadowContainer.classList.toggle('navbar-shadow-active')
 
-    })
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ''
+
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+        });
+
+        burger.classList.toggle('burger-active');
+    });
 }
 
 navSlide();
